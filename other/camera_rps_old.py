2dfbc9263a141f14b8cb73ce6ac79f095a7f1dd0
Kevin Docherty <kevdocherty@gmail.com>
2023-12-26 14:17:21 +0000

    Adds a countdown timer to inform the user to make their choice

--- other/camera_rps_old.py
+++ other/camera_rps_old.py
@@ -9,16 +9,11 @@ import cv2
 from keras.models import load_model
 import numpy as np
 from random import randint
-import time
 
 def get_prediction():
     """
     This function uses a Keras model, created using Teachable Machine,
-    to recognise whether the user is holding up rock, paper, or scissors
-    to the webcam.
-
-    The user is presented with a countdown from 10, at the end of which
-    the user must present their choice to the camera.
+    to recognise whether the user's hand is indicating rock, paper, or scissors
     """
     # load the computer vision model
     #model = load_model('keras_model.h5', compile=True)
@@ -31,40 +26,59 @@ def get_prediction():
     # - the fourth dimension represents the number of color channels in the image, = 3 (red, green, and blue)
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-    t1 = time.time()
-    countdown = 10
-    count = []
-    labels = ['rock', 'paper', 'scissors', 'nothing']
+    # create an empty np array to append each prediction as a separate row
+    predictions = np.ndarray(shape=(1, 4))
 
-    while True:
-        #countdown_displayed = False
-        t2 = time.time()
-        time_elapsed = t2 - t1
-        if time_elapsed > countdown:
-            print("time's up!")
+    # collect an np.array of 50 rows x 4 columns,
+    # where each row element is a float representing the probability of
+    # rock, paper, scissors, nothing, respectively
+    for i in range(50):
+        ret, frame = cap.read()
+        resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+        image_np = np.array(resized_frame)
+        normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
+        data[0] = normalized_image
+        prediction = model.predict(data)
+        cv2.imshow('frame', frame)
+        # Press q to close the window
+
+        # debug statements...
+        #print(type(prediction))
+        #print(prediction.shape)
+
+        print(np.round(prediction[0],2))
+        # append each 'prediction' to 'predictions'
+        if i == 0:
+            predictions = prediction
+        else:
+            predictions = np.vstack([np.round(prediction[0],2), predictions])
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-        if time_elapsed % 1 < 0.01 and int(time_elapsed) not in count:
-            count.append(int(time_elapsed))
-            #print(int(countdown - time_elapsed))
-            print(f"Time remaining: {countdown - count[-1]} seconds")
 
-            # capture an image from the webcam
-            # and classify as rock, paper, or scissors
-            # using the Keras model
-            ret, frame = cap.read()
-            resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
-            image_np = np.array(resized_frame)
-            normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
-            data[0] = normalized_image
-            prediction = model.predict(data)
-            cv2.imshow('frame', frame)
-            # Press q to close the window
+    # debug statements
+    #print(predictions)
+    #print(np.sum(predictions, axis=0))
 
-            user_choice = labels[np.argmax(prediction)]
+    # sum each column of predictions, and return the most probable result
+    # according to which has the maximum value
+    totals = np.sum(predictions, axis=0)
+    if np.argmax(totals) == 0:
+        #print('rock')
+        return 'rock'
+    elif np.argmax(totals) == 1:
+        #print('paper')
+        return 'paper'
+    elif np.argmax(totals) == 2:
+        #print('scissors')
+        return 'scissors'
+    else:
+        #print('nothing')
+        return 'nothing'
 
-            print(f"Your choice is: {user_choice}")
-    #print(count)
-    return(user_choice)
+    # After the loop release the cap object
+    cap.release()
+    # Destroy all the windows
+    cv2.destroyAllWindows()
 
 def get_computer_choice():
     """
@@ -113,12 +127,10 @@ def play():
     This function calls the other functions to play a game of rock-paper-scissors
     """
     computer_choice = get_computer_choice()
-    #user_choice = get_user_choice()
-    #user_choice = get_prediction()
-    user_choice = get_prediction()
-
     print(f"The computer's choice is: {computer_choice}")
-    print(f"Your choice is: {user_choice}")
+    #user_choice = get_user_choice()
+    user_choice = get_prediction()
+    print(f"The user's choice is: {user_choice}")
     get_winner(computer_choice, user_choice)
 
 if __name__ == '__main__':
